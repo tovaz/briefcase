b71b39a08c993292294318b24f29ea536556dff4
William Valdez <williamvaldez@outlook.com>
2021-07-04 10:05:47 +0200

    Exchange envirioment to nuveapps.com

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   appToken: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNjI0MTE3MTM3LCJleHAiOjE2MjY3MDkxMzd9.nTgA5wX4egKDmKvbtgn9uWZP9xUDONLs9AXi1pMEZS8',
-  apiServer: 'https://williamvaldez.clever-yonath.82-223-23-80.plesk.page',
-  uploadUrl: 'https://williamvaldez.clever-yonath.82-223-23-80.plesk.page'
+  apiServer: 'https://williamvaldez.nuveapps.com',
+  uploadUrl: 'https://williamvaldez.nuveapps.com'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,8 +5,8 @@
 export const environment = {
   production: false,
   appToken: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNjI0MTE3MTM3LCJleHAiOjE2MjY3MDkxMzd9.nTgA5wX4egKDmKvbtgn9uWZP9xUDONLs9AXi1pMEZS8',
-  apiServer: 'https://williamvaldez.clever-yonath.82-223-23-80.plesk.page',
-  uploadUrl: 'https://williamvaldez.clever-yonath.82-223-23-80.plesk.page'
+  apiServer: 'https://williamvaldez.nuveapps.com',
+  uploadUrl: 'https://williamvaldez.nuveapps.com'
 };
 
 /*
