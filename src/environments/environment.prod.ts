0b07cf77a75c2c00286b1e20669cacc949a9fd28
William Valdez <williamvaldez@outlook.com>
2021-07-12 16:38:01 +0200

    Dominio de api cambiado

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   appToken: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNjI0MTE3MTM3LCJleHAiOjE2MjY3MDkxMzd9.nTgA5wX4egKDmKvbtgn9uWZP9xUDONLs9AXi1pMEZS8',
-  apiServer: 'https://williamvaldez.nuveapps.com',
-  uploadUrl: 'https://williamvaldez.nuveapps.com'
+  apiServer: 'https://portafolioback.nuveapps.com',
+  uploadUrl: 'https://portafolioback.nuveapps.com'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,8 +5,8 @@
 export const environment = {
   production: false,
   appToken: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNjI0MTE3MTM3LCJleHAiOjE2MjY3MDkxMzd9.nTgA5wX4egKDmKvbtgn9uWZP9xUDONLs9AXi1pMEZS8',
-  apiServer: 'https://williamvaldez.nuveapps.com',
-  uploadUrl: 'https://williamvaldez.nuveapps.com'
+  apiServer: 'https://portafolioback.nuveapps.com',
+  uploadUrl: 'https://portafolioback.nuveapps.com'
 };
 
 /*
