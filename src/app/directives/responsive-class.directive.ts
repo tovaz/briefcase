c5020122b15166a421e9caa9ca3e0f87e1bb2e95
tovaz <williamvaldez@outlook.com>
2025-11-10 14:54:53 +0100

    small fixes and build

--- src/app/directives/responsive-class.directive.ts
+++ src/app/directives/responsive-class.directive.ts
@@ -1,15 +1,28 @@
 import { Directive, ElementRef, Input, OnInit, OnDestroy, Renderer2 } from '@angular/core';
 
 @Directive({
-  selector: '[ngClass.xs], [ngClass.sm], [ngClass.md], [ngClass.lg], [ngClass.xl]',
+  selector: '[ngClass.xs], [ngClass.sm], [ngClass.md], [ngClass.lg], [ngClass.xl], [ngClass.lt-sm], [ngClass.lt-md], [ngClass.lt-lg], [ngClass.lt-xl], [ngClass.gt-xs], [ngClass.gt-sm], [ngClass.gt-md], [ngClass.gt-lg]',
   standalone: false
 })
 export class ResponsiveClassDirective implements OnInit, OnDestroy {
+  // Breakpoints exactos
   @Input('ngClass.xs') ngClassXs: string | string[] | object = '';
   @Input('ngClass.sm') ngClassSm: string | string[] | object = '';
   @Input('ngClass.md') ngClassMd: string | string[] | object = '';
   @Input('ngClass.lg') ngClassLg: string | string[] | object = '';
   @Input('ngClass.xl') ngClassXl: string | string[] | object = '';
+
+  // Less than (menor que)
+  @Input('ngClass.lt-sm') ngClassLtSm: string | string[] | object = '';
+  @Input('ngClass.lt-md') ngClassLtMd: string | string[] | object = '';
+  @Input('ngClass.lt-lg') ngClassLtLg: string | string[] | object = '';
+  @Input('ngClass.lt-xl') ngClassLtXl: string | string[] | object = '';
+
+  // Greater than (mayor que)
+  @Input('ngClass.gt-xs') ngClassGtXs: string | string[] | object = '';
+  @Input('ngClass.gt-sm') ngClassGtSm: string | string[] | object = '';
+  @Input('ngClass.gt-md') ngClassGtMd: string | string[] | object = '';
+  @Input('ngClass.gt-lg') ngClassGtLg: string | string[] | object = '';
 
   private mediaQueryListeners: Array<{ query: MediaQueryList; handler: () => void }> = [];
 
@@ -31,11 +44,24 @@ export class ResponsiveClassDirective implements OnInit, OnDestroy {
 
   private setupResponsiveClasses() {
     const breakpoints = [
+      // Breakpoints exactos
       { name: 'xs', query: '(max-width: 599px)', classes: this.ngClassXs },
       { name: 'sm', query: '(min-width: 600px) and (max-width: 959px)', classes: this.ngClassSm },
       { name: 'md', query: '(min-width: 960px) and (max-width: 1279px)', classes: this.ngClassMd },
       { name: 'lg', query: '(min-width: 1280px) and (max-width: 1919px)', classes: this.ngClassLg },
-      { name: 'xl', query: '(min-width: 1920px)', classes: this.ngClassXl }
+      { name: 'xl', query: '(min-width: 1920px)', classes: this.ngClassXl },
+
+      // Less than (menor que)
+      { name: 'lt-sm', query: '(max-width: 599px)', classes: this.ngClassLtSm }, // < 600px
+      { name: 'lt-md', query: '(max-width: 959px)', classes: this.ngClassLtMd }, // < 960px
+      { name: 'lt-lg', query: '(max-width: 1279px)', classes: this.ngClassLtLg }, // < 1280px
+      { name: 'lt-xl', query: '(max-width: 1919px)', classes: this.ngClassLtXl }, // < 1920px
+
+      // Greater than (mayor que)
+      { name: 'gt-xs', query: '(min-width: 600px)', classes: this.ngClassGtXs }, // >= 600px
+      { name: 'gt-sm', query: '(min-width: 960px)', classes: this.ngClassGtSm }, // >= 960px
+      { name: 'gt-md', query: '(min-width: 1280px)', classes: this.ngClassGtMd }, // >= 1280px
+      { name: 'gt-lg', query: '(min-width: 1920px)', classes: this.ngClassGtLg }  // >= 1920px
     ];
 
     breakpoints.forEach(breakpoint => {
--- src/app/directives/responsive-style.directive.ts
+++ src/app/directives/responsive-style.directive.ts
@@ -1,15 +1,28 @@
 import { Directive, ElementRef, Input, OnInit, OnDestroy, Renderer2 } from '@angular/core';
 
 @Directive({
-  selector: '[ngStyle.xs], [ngStyle.sm], [ngStyle.md], [ngStyle.lg], [ngStyle.xl]',
+  selector: '[ngStyle.xs], [ngStyle.sm], [ngStyle.md], [ngStyle.lg], [ngStyle.xl], [ngStyle.lt-sm], [ngStyle.lt-md], [ngStyle.lt-lg], [ngStyle.lt-xl], [ngStyle.gt-xs], [ngStyle.gt-sm], [ngStyle.gt-md], [ngStyle.gt-lg]',
   standalone: false
 })
 export class ResponsiveStyleDirective implements OnInit, OnDestroy {
+  // Breakpoints exactos
   @Input('ngStyle.xs') ngStyleXs: { [key: string]: any } | string = {};
   @Input('ngStyle.sm') ngStyleSm: { [key: string]: any } | string = {};
   @Input('ngStyle.md') ngStyleMd: { [key: string]: any } | string = {};
   @Input('ngStyle.lg') ngStyleLg: { [key: string]: any } | string = {};
   @Input('ngStyle.xl') ngStyleXl: { [key: string]: any } | string = {};
+
+  // Less than (menor que)
+  @Input('ngStyle.lt-sm') ngStyleLtSm: { [key: string]: any } | string = {};
+  @Input('ngStyle.lt-md') ngStyleLtMd: { [key: string]: any } | string = {};
+  @Input('ngStyle.lt-lg') ngStyleLtLg: { [key: string]: any } | string = {};
+  @Input('ngStyle.lt-xl') ngStyleLtXl: { [key: string]: any } | string = {};
+
+  // Greater than (mayor que)
+  @Input('ngStyle.gt-xs') ngStyleGtXs: { [key: string]: any } | string = {};
+  @Input('ngStyle.gt-sm') ngStyleGtSm: { [key: string]: any } | string = {};
+  @Input('ngStyle.gt-md') ngStyleGtMd: { [key: string]: any } | string = {};
+  @Input('ngStyle.gt-lg') ngStyleGtLg: { [key: string]: any } | string = {};
 
   private mediaQueryListeners: Array<{ query: MediaQueryList; handler: () => void }> = [];
   private appliedStyles: Set<string> = new Set();
@@ -32,11 +45,24 @@ export class ResponsiveStyleDirective implements OnInit, OnDestroy {
 
   private setupResponsiveStyles() {
     const breakpoints = [
+      // Breakpoints exactos
       { name: 'xs', query: '(max-width: 599px)', styles: this.ngStyleXs },
       { name: 'sm', query: '(min-width: 600px) and (max-width: 959px)', styles: this.ngStyleSm },
       { name: 'md', query: '(min-width: 960px) and (max-width: 1279px)', styles: this.ngStyleMd },
       { name: 'lg', query: '(min-width: 1280px) and (max-width: 1919px)', styles: this.ngStyleLg },
-      { name: 'xl', query: '(min-width: 1920px)', styles: this.ngStyleXl }
+      { name: 'xl', query: '(min-width: 1920px)', styles: this.ngStyleXl },
+
+      // Less than (menor que)
+      { name: 'lt-sm', query: '(max-width: 599px)', styles: this.ngStyleLtSm }, // < 600px
+      { name: 'lt-md', query: '(max-width: 959px)', styles: this.ngStyleLtMd }, // < 960px
+      { name: 'lt-lg', query: '(max-width: 1279px)', styles: this.ngStyleLtLg }, // < 1280px
+      { name: 'lt-xl', query: '(max-width: 1919px)', styles: this.ngStyleLtXl }, // < 1920px
+
+      // Greater than (mayor que)
+      { name: 'gt-xs', query: '(min-width: 600px)', styles: this.ngStyleGtXs }, // >= 600px
+      { name: 'gt-sm', query: '(min-width: 960px)', styles: this.ngStyleGtSm }, // >= 960px
+      { name: 'gt-md', query: '(min-width: 1280px)', styles: this.ngStyleGtMd }, // >= 1280px
+      { name: 'gt-lg', query: '(min-width: 1920px)', styles: this.ngStyleGtLg }  // >= 1920px
     ];
 
     breakpoints.forEach(breakpoint => {
